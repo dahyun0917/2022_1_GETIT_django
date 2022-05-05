83663227aced0a1c9982fdf1ad545d018aef5163
dahyun0917 <dahyun0917@naver.com>
2022-05-05 13:01:25 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -11,4 +12,15 @@ def index(request):
         {
             'posts': posts,
         }
-    ) 
+    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
