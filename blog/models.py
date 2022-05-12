efe82af8681d2eec6b39df420c401d7402855892
dahyun0917 <dahyun0917@naver.com>
2022-05-12 21:10:11 +0900

    미분류 분류 카테고리 완성(다대일)

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,20 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique = True)
+    slug = models.SlugField(max_length=200, unique= True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+    
+    class Meta:
+        verbose_name_plural='Categories'
+    
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=30)
@@ -18,6 +32,8 @@ class Post(models.Model):
     
     # author : 추후 작성 예정
     author = models.ForeignKey(User,null=TRUE,on_delete=models.SET_NULL)
+    
+    category = models.ForeignKey(Category,null=True, blank=True,on_delete=models.SET_NULL)
     
     def __str__(self):
         return f'[{self.pk}]{self.title}::{self.author}'
