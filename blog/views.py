2523fecce81e4d3e08815fc0ae7e3769fcb0c003
dahyun0917 <dahyun0917@naver.com>
2022-05-16 00:15:43 +0900

    포스트 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
 #from re import template
 #from django.shortcuts import render
-from multiprocessing import context
-from unicodedata import category
 from django.shortcuts import render,redirect
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView , UpdateView
 from .models import Post, Category,Tag
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -33,13 +32,25 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
     
-    def form_vaild(self, form):
+    def form_valid(self, form):
         current_user=self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author=current_user
             return super(PostCreate,self).form_valid(form)
         else:
             return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin,UpdateView):
+    model=Post
+    fields=['title','hook_text','content','head_image','file_upload','category','tags'] 
+    
+    template_name ='blog/post_update_form.html'
+    
+    def dispatch(self,request,*args,**kwargs):
+        if request.user.is_authenticated and request.user==self.get_object().author:
+            return super(PostUpdate,self).dispatch(request,*args,**kwargs)
+        else:
+            raise PermissionDenied
     
 def category_page(request,slug):
     if slug=='no_category':
